5c04f3d211fe8202c197d50ede0c7cb259804dfe
Your Name <you@example.com>
2024-02-06 14:43:15 +0530

    first commit

---
+++ server/SampleData.js
@@ -0,0 +1,49 @@
+// Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description: "LOREM IPSUm",
+    status: "Complete",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Fashion Website",
+    description: "LOREM IPSUm",
+    status: "Complete",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Food Website",
+    description: "LOREM IPSUm",
+    status: "In Progress",
+  },
+];
+
+//Clients
+
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "tonay@stark.com",
+    phone: "8545465654",
+  },
+  {
+    id: "2",
+    name: "Mehul",
+    email: "mehul@stark.com",
+    phone: "8545465654",
+  },
+  {
+    id: "3",
+    name: "Sumit Stark",
+    email: "sumit@stark.com",
+    phone: "8545465654",
+  },
+];
+
+module.exports = { projects, clients };
